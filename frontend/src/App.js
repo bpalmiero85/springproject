80377d645cafc29f438dc444dd20432deba094ea
bpalmiero85 <bpalmiero85@gmail.com>
2024-07-07 23:53:10 -0400

    User registration complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import UserInfo from './pages/UserInfo'; 
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import UserInfo from "./pages/UserInfo";
+import RegisterPage from "./pages/RegisterPage";
+import WelcomePage from "./pages/WelcomePage";
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/userinfo" element={<UserInfo />} />
-                {/* Add other routes here */}
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/userinfo" element={<UserInfo />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/welcome" element={<WelcomePage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
