2851c090e134e287b9f45ba6513e83a235527e4a
bpalmiero85 <bpalmiero85@gmail.com>
2024-07-09 21:53:21 -0400

    Create navbar and logout

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,96 +1,95 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
-import "../styles/LoginPage.css"
+import "../styles/LoginPage.css";
 
 const LoginPage = () => {
- const [username, setUsername] = useState("");
- const [password, setPassword] = useState("");
- const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [, setCookie] = useCookies(["user"]);
 
- const [cookie, setCookie, removeCookie] = useCookies(["user"]);
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
- const handleLogin = async (e) => {
-  e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:8080/user/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
 
-  try {
-    const response = await fetch("http://localhost:8080/user/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+        credentials: "include",
+      });
 
-      body: JSON.stringify({
-        username,
-        password,
-      }),
-      creentials: "include",
-    });
-
-    if(response.ok){
-      const data = await response.text();
-      setCookie("user", username);
-      navigate(`/homepage?username=${username}`);
-    } else {
-      const errorText = await response.text();
-      console.error(`Error: ${errorText}`);
+      if (response.ok) {
+        const data = await response.text();
+        setCookie("user", username);
+        navigate(`/homepage?username=${username}`);
+      } else {
+        const errorText = await response.text();
+        console.error(`Error: ${errorText}`);
+      }
+    } catch (error) {
+      console.error("Error logging in", error);
     }
-  } catch (error) {
-    console.error("Error logging in", error);
-  }
- };
+  };
 
- const handleRegister = () => {
-  navigate("/register");
- };
+  const handleRegister = () => {
+    navigate("/register");
+  };
 
- return (
-  <div className="login-background">
-  <div className="login-title">
-    <h2>Login</h2>
-  </div>
-  <div className="login-container">
-  <form onSubmit={handleLogin}>
-    <div className="login-fields">
-      <div className="login-username">
-        <div className="login-password">
-          <label className="login-label">Username: </label>
-          <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          className="login-input"
-          />
-        </div>
-        <div className="login-password">
-          <label className="login-label">Password: </label>
-          <input 
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="login-input"
-          />
-        </div>
+  return (
+    <div className="login-background">
+      <div className="login-title">
+        <h2>Login</h2>
       </div>
-      <button 
-        type="submit"
-        className="login-button"
-        onClick={handleLogin}
-        >
-          Login
-        </button>
-        <button 
-          type="submit"
-          className="login-button"
-          onClick={handleRegister}
-          >
-          Sign Up
-          </button>
+      <div className="login-container">
+        <form onSubmit={handleLogin}>
+          <div className="login-fields">
+            <div className="login-username">
+              <div className="login-password">
+                <label className="login-label">Username: </label>
+                <input
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="login-input"
+                />
+              </div>
+              <div className="login-password">
+                <label className="login-label">Password: </label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="login-input"
+                />
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="login-button"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+            <button
+              type="submit"
+              className="login-button"
+              onClick={handleRegister}
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  </form>
-</div>
-</div>
- );
+  );
 };
 
 export default LoginPage;
--- frontend/src/pages/LogoutPage.jsx
+++ frontend/src/pages/LogoutPage.jsx
@@ -1,65 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { useCookies } from 'react-cookie';
-import { useHistory, useLocation } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import Navbar from "../components/Navbar";
 import "../styles/LogoutPage.css";
+import FetchUser from "../components/FetchUser";
 
-const Logout = () => {
-    const location = useLocation();
-    const searchParams = new URLSearchParams(location.search);
-    const username = searchParams.get('username');
-    const [user, setUser] = useState(null);
-    const [error, setError] = useState(null);
-    const navigate = useNavigate();
+const LogoutPage = () => {
+  const { user, error } = FetchUser();
 
-    useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const response = await fetch(`http://localhost:8080/user/userinfo?username=${username}`, {
-                    method: "GET",
-                    credentials: "include",
-                });
 
-                if (response.ok) {
-                    const userData = await response.json();
-                   setUser(userData);
-                   navigate(`/logout?username=${username}`)
-                } else {
-                    const errorMessage = `Error fetching user info: ${response.statusText}`;
-                  
-                    setError(errorMessage);
-                }
-            } catch (error) {
-                const errorMessage = `Error fetching user info: ${error.message}`;
-                
-                setError(errorMessage);
-            }
-        };
+  if (error) {
+    return <div>{error}</div>;
+  }
 
-        if (username) {
-            fetchUser();
-        } else {
-            setError('Username parameter is missing');
-        }
-    }, [username]);
+  if (!user) {
+    return <div>Loading...</div>;
+  }
 
-    if (error) {
-        return <div>{error}</div>;
-    }
 
-    if (!user) {
-        return <div>Loading...</div>;
-    }
-
-    return (
-        <div className="logout-container">
-            <Navbar />
-            <div className="logout-message">
-                {user && <h3>We hope to see you again soon, <span>{user.firstName}</span>. Goodbye!</h3>}
-            </div>
-        </div>
-    );
+  return (
+    <div className="logout-container">
+      <Navbar />
+      <div className="logout-message">
+        <h3>
+          We hope to see you again soon, <span>{user.firstName}</span>. Goodbye!
+        </h3>
+      </div>
+    </div>
+  );
 };
 
-export default Logout;
+export default LogoutPage;
