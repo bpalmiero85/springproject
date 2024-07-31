49e3a2af0cc2ff364831eef61ce58bb8c8c3c8ad
bpalmiero85 <bpalmiero85@gmail.com>
2024-07-31 18:59:49 -0400

    Fixed verification redirect

--- src/main/java/com/example/springproject/Models/UserSession.java
+++ src/main/java/com/example/springproject/Models/UserSession.java
@@ -3,20 +3,26 @@ package com.example.springproject.Models;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @Setter
 @Getter
 @NoArgsConstructor
 @Entity
 public class UserSession {
+
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private String sessionId;
-  private String username;
+
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long userId;
+  
   private LocalDateTime lastAccessed;
 }
